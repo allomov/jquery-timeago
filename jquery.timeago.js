3098539ba77030f0773096771045e50f4ed6bcf3
code1n <lomov.as@gmail.com>
2011-05-04 12:44:45 +0300

    adds language direction to the settings, opportunity to set time intervals in settings in words like 'an hour and 2 minutes', also adds to settings interval to show normal date after

--- jquery.timeago.js
+++ jquery.timeago.js
@@ -27,7 +27,9 @@
 
   $.extend($.timeago, {
     settings: {
-      refreshMillis: 60000,
+      showNormalDateAfter: '7 days',    // default Infinity
+      refreshInterval: 'minute',
+      direction: 'ltr',
       allowFuture: false,
       strings: {
         prefixAgo: null,
@@ -35,30 +37,21 @@
         suffixAgo: "ago",
         suffixFromNow: "from now",
         seconds: "less than a minute",
-        minute: "about a minute",
+        minute: "a minute",
         minutes: "%d minutes",
-        hour: "about an hour",
-        hours: "about %d hours",
+        hour: "an hour",
+        hours: "%d hours",
         day: "a day",
         days: "%d days",
-        month: "about a month",
+        month: "a month",
         months: "%d months",
-        year: "about a year",
+        year: "a year",
         years: "%d years",
         numbers: []
       }
     },
     inWords: function(distanceMillis) {
       var $l = this.settings.strings;
-      var prefix = $l.prefixAgo;
-      var suffix = $l.suffixAgo;
-      if (this.settings.allowFuture) {
-        if (distanceMillis < 0) {
-          prefix = $l.prefixFromNow;
-          suffix = $l.suffixFromNow;
-        }
-        distanceMillis = Math.abs(distanceMillis);
-      }
 
       var seconds = distanceMillis / 1000;
       var minutes = seconds / 60;
@@ -66,26 +59,37 @@
       var days = hours / 24;
       var years = days / 365;
 
-      function substitute(stringOrFunction, number) {
-        var string = $.isFunction(stringOrFunction) ? stringOrFunction(number, distanceMillis) : stringOrFunction;
+      function substitute(timeUnits , number) {
+        var prefix = $.isFunction($l.prefixAgo) ? $l.prefixAgo(timeUnits) : $l.prefixAgo;
+        var suffix = $.isFunction($l.suffixAgo) ? $l.suffixAgo(timeUnits) : $l.suffixAgo;
+
+        if ($t.settings.allowFuture) {
+          if (distanceMillis < 0) {
+            prefix = $l.prefixFromNow;
+            suffix = $l.suffixFromNow;
+          }
+          distanceMillis = Math.abs(distanceMillis);
+        }
+        var localeStringOrFunc = $l[timeUnits];
+        var string = $.isFunction(localeStringOrFunc) ? localeStringOrFunc(number, distanceMillis) : localeStringOrFunc;
         var value = ($l.numbers && $l.numbers[number]) || number;
-        return string.replace(/%d/i, value);
+        var words = string.replace(/%d/i, value);
+        return $.trim(($t.settings.direction == 'ltr' ? [prefix, words, suffix] : [suffix, words, prefix]).join(" "));
       }
 
-      var words = seconds < 45 && substitute($l.seconds, Math.round(seconds)) ||
-        seconds < 90 && substitute($l.minute, 1) ||
-        minutes < 45 && substitute($l.minutes, Math.round(minutes)) ||
-        minutes < 90 && substitute($l.hour, 1) ||
-        hours < 24 && substitute($l.hours, Math.round(hours)) ||
-        hours < 48 && substitute($l.day, 1) ||
-        days < 30 && substitute($l.days, Math.floor(days)) ||
-        days < 60 && substitute($l.month, 1) ||
-        days < 365 && substitute($l.months, Math.floor(days / 30)) ||
-        years < 2 && substitute($l.year, 1) ||
-        substitute($l.years, Math.floor(years));
-
-      return $.trim([prefix, words, suffix].join(" "));
+      return seconds < 45 && substitute('seconds', Math.round(seconds)) ||
+        seconds < 90 && substitute('minute', 1) ||
+        minutes < 45 && substitute('minutes', Math.round(minutes)) ||
+        minutes < 90 && substitute('hour', 1) ||
+        hours < 24 && substitute('hours', Math.round(hours)) ||
+        hours < 48 && substitute('day', 1) ||
+        days < 30 && substitute('days', Math.floor(days)) ||
+        days < 60 && substitute('month', 1) ||
+        days < 365 && substitute('months', Math.floor(days / 30)) ||
+        years < 2 && substitute('year', 1) ||
+        substitute('years', Math.floor(years));
     },
+      
     parse: function(iso8601) {
       var s = $.trim(iso8601);
       s = s.replace(/\.\d\d\d+/,""); // remove milliseconds
@@ -103,22 +107,49 @@
   });
 
   $.fn.timeago = function() {
+
     var self = this;
+    var $s = $t.settings;
+
+    $s.showNormalDateAfter = getMillis($s.showNormalDateAfter);
+    $s.refreshInterval = getMillis($s.refreshInterval);
+
     self.each(refresh);
 
-    var $s = $t.settings;
-    if ($s.refreshMillis > 0) {
-      setInterval(function() { self.each(refresh); }, $s.refreshMillis);
+
+
+    if ($s.refreshInterval > 0) {
+      setInterval(function() { self.each(refresh); }, $s.refreshInterval);
     }
+
     return self;
   };
 
   function refresh() {
     var data = prepareData(this);
-    if (!isNaN(data.datetime)) {
-      $(this).text(inWords(data.datetime));
+    if (!isNaN(data.datetime)){
+      var dist = distance(data.datetime);
+      $(this).text(dist < $t.settings.showNormalDateAfter ? distanceInWords(dist) : $(this).attr('title'));
     }
     return this;
+  }
+
+  var secondsCount = {second: 1, minute: 60, hour: 3600, day: 86400, week: 604800, month: 2592000, year: 31536000}; // month has 30 days here
+  function getMillis(stringOrMillis){
+    switch (typeof(stringOrMillis)) {
+      case 'string' :
+        var millis = 0;
+        for (var timeUnit in secondsCount) {
+          var re = stringOrMillis.match(new RegExp('(\\d+\\s+)?' + timeUnit + 's?'));
+          if (re) {
+              var timeUnitsCount = parseInt(re[1]);
+              millis += (isNaN(timeUnitsCount) ? 1 : timeUnitsCount) * secondsCount[timeUnit];
+          }
+        }
+        return millis * 1000;
+      case 'number' : return stringOrMillis;
+      default : return 0;
+    }
   }
 
   function prepareData(element) {
@@ -134,7 +165,11 @@
   }
 
   function inWords(date) {
-    return $t.inWords(distance(date));
+    return distanceInWords(distance(date));
+  }
+  
+  function distanceInWords(distance) {
+    return $t.inWords(distance);
   }
 
   function distance(date) {
